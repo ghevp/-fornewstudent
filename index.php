5688cdc27e574a2f53d1a0fcfe269cc142e022a4
gue <main@ghevp.com>
2022-02-25 19:19:17 +0900

    制作

--- index.php
+++ index.php
@@ -8,14 +8,37 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Kosugi&display=swap" rel="stylesheet">   
         <link rel="stylesheet" href="css/style.css"><!--後でphpのif文の作成-->
-        <title>新入生向けネットリテラシー啓発（仮称）</title> <!--後で変更-->
+      <title> <?php 
+      if (isset($title) == "") {
+        echo "静浜学生情報サイト";
+    } else {
+        echo "" . $title . " - 静浜学生情報サイト";
+    }
+       ?>
+      </title><!--後で変更-->
     </head>
     <body>
-    <div class="content">
-    <div class="main">
-    <h1>新入生向けネットリテラシー啓発（仮称）</h1>
-    <div class="main2">
-        <?php
+   <p>test</p>
+    <header class="header">
+        <div class="header-inner">
+            <a href=""><img src="img/logo.png"></a>
+            <div class="header-mobile-button">
+                <button class="toggle-menu-button"></button>
+            </div>
+            <div class="header-site-menu">
+                <nav class="site-menu">
+                    <ul>
+                        <li>Home</li>
+                        <li>新入生の皆さんへ</li>
+                        <li>おしらせ</li>
+                        <li>困った時</li><!--増やすときはボタンの使用で変更-->
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <main class="main">
+    <?php
         date_default_timezone_set('Asia/Tokyo');
         $month = date("m");
       
@@ -27,57 +50,22 @@
                   echo"<p>このウェブサイトでは、新入生だけではなく、既に在学中の人にとっても読んで損はないコンテンツを公開しているのでよろしくお願いします。</p>";
               };
     ?>
-    <h2>
-        はじめに
-            </h2>
-    <details>
-        <summary>
-            
-            静岡キャンパスの人へ
-            
-        </summary>
-        <h2>
-            知っておくべき事
-        </h2>
-        <p>
-        人文社会科学部のプレ入学式は微妙<br>
-        家は朝寝坊しても平気がいいのなら大谷、生活が便利なのがいいのなら小鹿<br>
-        夜の静大前ファミマに注意<br>
-        </p>
-    </details>
-    <details>
-        <summary>
-        
-            浜松キャンパスの人へ
-        
-        </summary>
-        <h2>
-            知っておくべき事
-        </h2>
-        <p>
-    <a href="https://gakujochan.com"target="blank"><img src="img/ad/slide_top.png" width="100%"></a>
-    </p>
-    </details>
-    </div>
+    </main>
+    <footer class="footer">
 
-</div>
-    <div class="senden">
-    <p>
-        宣伝欄
-    </p>
-    <p>
-        学情チャンプロジェクトの宣伝<br></p>
-    <p>
-    <a href="https://gakujochan.com"target="blank"><img src="img/ad/slide_top.png" width="100%"></a>
-    </p>
-    <?php
-    date_default_timezone_set('Asia/Tokyo');
-    echo "<p>大学祭実行委員会の宣伝</p>"
-    ?>
-    </div>
-    </div>
-    <div class="bottom">
-    <p>copyright©<a href="https://ghevp.com">ぐゑ</a> all rights reserved. </p>
-    </div>
-    </body>
+    
+    <nav class="site-menu">
+                    <ul>
+                        <li>Home</li>
+                        <li>新入生の皆さんへ</li>
+                        <li>おしらせ</li>
+                        <li>困った時</li><!--増やすときはボタンの使用で変更-->
+                    </ul>
+                </nav>
+
+                <a class="footer-logo" href=""><img src="img/logo.png"></a>
+                <p class="attention">注意:このウェブサイトは静岡大学をはじめとした大学・企業・その他機関とは一切関係ありません。</p>
+                <p class="copyright"><small>copyright&copy;<a href="https://ghevp.com">ぐゑ</a>all rights reserved.</small></p>
+    </footer>   
+  </body>
 </html>
