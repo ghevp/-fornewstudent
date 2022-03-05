7a11be4024c8adec346e9edc7ad9653cfa7f763e
gue <main@ghevp.com>
2022-03-06 03:49:02 +0900

    多分完成

--- index.php
+++ index.php
@@ -18,7 +18,15 @@
 ?>
     <head>
         <meta charset="UTF-8">
-       
+       <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0P8XN7BCL8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0P8XN7BCL8');
+</script>
         <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -36,7 +44,7 @@
       </title><!--後で変更-->
     </head>
     <body>
-   <p>test</p>
+   
     <header class="header">
         <div class="header-inner">
             <a href="index.php?page=home"><img src="img/logo.png"></a>
@@ -49,7 +57,7 @@
                         <li><a href="index.php?page=home">ホーム</a></li>
                         <li><a href="index.php?page=newstudents">新入生の皆さんへ</a></li>
                         <li><a href="index.php?page=notice">おしらせ</a></li>
-                        <li><a href="index.php?page=question">困った時</a></li><!--増やすときはボタンの使用で変更-->
+                        <li><a href="index.php?page=question">質問など</a></li><!--増やすときはボタンの使用で変更-->
                     </ul>
                 </nav>
             </div>
@@ -89,7 +97,11 @@
               <ul>
                 <li>ShizudaiTech</li>
               <p>非常に不便であると有名な静大の学情を便利にしてくれるツールをまとめたウェブサイトです。<br>
-              </p>
+              アクセスは<a href=","https://shizudai-tech.netlify.app/",">こちら</a>から！</p>
+              <li>楽天みんなのキャンパス</li>
+              <p>授業のレビューサイトです。授業の評価や内容等を閲覧することができます。<br>
+              授業を取る前に一度見てみるのはいかがでしょうか？<br>
+              アクセスは<a href=","https://campus.nikki.ne.jp/?module=lesson&univ=%C0%C5%B2%AC%C2%E7%B3%D8",">こちら</a>から！</p>
               </div>";
             };
         if("newstudents"==$page){
@@ -132,7 +144,33 @@
             <p>TwitterやYouTubeなどのSNSで生協PCはダメだ!等の情報が散見されますが、これは<b>人によります</b>。<br>
             <b>PCカバーをつけずに自転車のかごにPCを入れるような人</b>、<b>PCの画面部分を持ち、PCを持ち上げて運ぼうとする人</b>など極めて基礎的なコンピュータに対する知識がない人、モノを大切に扱う能力のない人は<b>非常に強固な保障の効いた生協PCの購入をおすすめします</b>。<br>
             <b>しかしながら</b>そうでない人に関しては生協以外を通して購入した方が金銭的な負担は低いと思われますし、何より自分に合ったパソコンを選ぶことができるので生協以外でのPC購入がおすすめです。<br>
-            ここでは、各学部ごとにお勧めのPCを紹介するとともに<b>絶対に避けたほうが良いPC</b>についてもご紹介します。</p>
+            ここでは、各学部ごとにお勧めのPCを紹介するとともに<b>絶対に避けたほうが良いPC</b>についてもご紹介します。<b>ただし</b>PCについて何もわからないという人は下の「PCスペック表の見方」をチェックしてからこれらを確認してください。<br>
+            また、昨今の半導体不足によりPCによっては納品に一か月以上かかるものもあります。<b>情報学部の人は遅くとも4月のPC設定会まで、それ以外の人は授業開始までにパソコンが手元に入るかを確認してから</b>購入してください。</p>
+            <details>
+            <summary>PCスペック表の見方(わからない方向け）</summary>
+            <h2>PCスペック表とは?</h2>
+            <p>PCの性能の事を「スペック」と呼びます。<br>
+            PCは単純比較することが難しい機械なので、部品ごとの性能を見ることによって初めて自分の求めている性能のPCを購入することができます。<br>
+            つまり、PCスペック表とはPCの様々な部品の性能を一覧にして見やすくしたものです。<br>
+            ここでは、そんな様々な部品についての知っていただき、PC購入の助けになれば幸いです。</p>
+            <h2>各部品の名称と機能等について</h2>
+            <ul>
+            <li>CPU</li>
+            <p>PCの頭脳に値する部品です。詳しい説明は割愛させていただきますが、Intel社のCoreシリーズや、AMD社のRyzenシリーズ等があり、それぞれのメーカーで、3,5,7,9の名前を付けた製品があります。数字の大きいほど性能が上がります。Intel社とAMD社の製品で比較するとIntel社の製品の方が安定性に長けており、同じ番号での比較であるとIntel社はやや性能が高いですが、お値段もその分お高めです。<br>
+            Intel社やAMD社はお互いに<b>CoreやRyzenではない</b>CPUを製造していますが、廉価版ゆえに性能は非常に低く、学業には向かないので購入は絶対にひかえてください。</p>
+            <li>メモリ</li>
+            <p>RAMとも呼ばれます。よく机の大きさに例えられますが、このメモリの量でどれだけのアプリケーション等を起動しておけるかが決まります。<br>
+            小さすぎると学業に必要なソフトを満足に扱えないので絶対に8GB以上のものを購入しましょう。</p>
+            <li>記憶量、記憶媒体</li>
+            <p>SSDやHDD、eMMCの文字と共に○○GBと表記されることが多いです。まず、SSDやHDD,eMMC等に関してですが、記憶媒体の形式をさしています。場合によっては転送速度が○○MB/sのような形で示されることもありますが、これが速いほど読み込みが早いことを示しています。<br>
+            これが速いとアプリやPC本体の立ち上げが高速になりますが、逆に遅いと満足にアプリを扱えない事が多々あるので低速な記憶媒体であるHDDやeMMCを搭載しているPCは絶対に購入しないようにしてください</p>
+            <li>画面サイズ</li>
+            <p>PCの画面の大きさを示しています。大きいほど作業効率は上がりますが、持ち運びが不便です。</p>
+            <li>GPU</li>
+            <p>単純計算に特化した部品です。大半のノートパソコンにはCPUに内蔵されていますが、ゲームや動画編集をしたい人は別でついているものを購入することをおすすめします。</p>
+
+
+            </details>
             <h2>絶対に購入してはいけないPC</h2>
             <p>下記の条件を<b>すべて満たさない</b>PCを購入することを強くお勧めします。
             <ul>
@@ -142,7 +180,7 @@
             Macを買うのは家庭のPCがMacである等、逆にWindows11搭載製品を買うと困る人だけにしましょう。<br>
             また、<b>教育学部美術専修又は地域創造学館アートマネジメントコース</b>の人はMacを使ってもいいかもしれません。詳しくは、「学部別おすすめPC」において記載します。<br>
             <b>工学部、情報学部の人はそのような場合であってもWindows11搭載のものを購入してください。</b>Windows11でないと授業を受けることが不可能、もしくは非常に困難な授業がたくさんあります。<br>
-            また、同様の理由でOSがwindowsであってもCPUがArm系のものは絶対に避けてください。</p>
+            また、同様の理由でOSがwindowsであってもCPUがArm系のものは絶対に避けてください。もちろん、<b>Windows11に対応していない古いCPU搭載のもの</b>も絶対にダメです</p>
             <li>・重量が2Kg以上のもの、画面サイズが15インチより大きいもの</li>
             <p>重量が重かったり、画面サイズが大きいと持ち運びが不便になります。<br>
             大学にPCを持ち込んで勉強することも多々ありますので購入するPCは2Kg以下かつ画面サイズが15インチ以下のものを選択しましょう。<br>
@@ -158,25 +196,29 @@
             <h2>学部別おすすめのPC</h2>
             <p>学科ごとにお勧めのPCを記載しています。<br>
             中の人はパソコンヲタクではありますが情報の不備等がある可能性があるので、あくまでも参考程度にご覧ください。</p>
+            
             <details>
             <summary>情報学部</summary>
             <p>先述しましたが、MacやArm系のCPUの製品は絶対に避けてください。授業では動かないソフトがありますので、最悪買い替えが必要になってしまいます。</p>
             <h2>ゲームも楽しみたい人へ</h2>
             <p>ゲームをするためにはグラフィックボードは必須です。ですので、グラフィックボード搭載のPCを買うとよいでしょうがFPS等の非常に高いスペックを要求されるゲームをする場合はデスクトップPCを別に購入することをお勧めします。<br>
             中程度のゲームであればグラフィックボード搭載のノートPCでも対応は可能です。<br>
-            そのような用途のPCを求めている人にはGeForce RTX™ 3050 Tiを搭載していながらも持ち運びが可能な軽量さを兼ね備えている<a href=","https://www.dell.com/ja-jp/shop/%E3%83%87%E3%83%AB%E3%81%AE%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3/inspiron-15-plus-%E3%83%97%E3%83%A9%E3%83%81%E3%83%8A/spd/inspiron-15-7510-laptop/cai215hspchs16on3ojp#features_section",">Inspiron 15 Plus</a>をおすすめします。</p>
+            そのような用途のPCを求めている人にはGeForce RTX™ 3050 Tiを搭載していながらも持ち運びが可能な軽量さを兼ね備えている<a href=","https://www.dell.com/ja-jp/shop/%E3%83%87%E3%83%AB%E3%81%AE%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3/inspiron-15-plus-%E3%83%97%E3%83%A9%E3%83%81%E3%83%8A/spd/inspiron-15-7510-laptop/cai215hspchs16on3ojp#features_section",">Inspiron 15 Plus</a>をおすすめします。ただし、少々大きいとはおもいます。</p>
             <h2>それ以外の人へ</h2>
-            <p>情報学部の授業では多量のメモリを必要とするソフトを使う事が多々あります。ですので、メモリ16GBを搭載した<a href=","https://www.dell.com/ja-jp/shop/cty/pdp/spd/inspiron-15-3511-laptop/cai3453hlwts16on3ojp?gacd=9643257-23732657-5785552-266271997-127759547&dgc=af&VEN1=/Vv6e0WKODg-*auGi30V6YDcCKerYpozIg&dclid=CPj1-bKCqvYCFQaNvAodUQoM7Q&nclid=DIBttjKlZEo-8QBoXQvCfzuePsqAVOgbZNvNUi6irqfJQk-ScuiSR2d27tOCOlWS#features_section","</a>Inspiron 15 3000</a>や<a href=","https://www.lenovo.com/jp/ja/kakaku/notebooks/ideapad/flex-5-series/IdeaPad-Flex-5-14ITL-05/p/82HS00X3JP",">IdeaPad Flex 550i</a>をおすすめします。</p>
+            <p>情報学部の授業では多量のメモリを必要とするソフトを使う事が多々あります。ですので、メモリ16GBを搭載した<a href=","https://www.dell.com/ja-jp/work/shop/%E3%83%87%E3%83%AB%E3%81%AE%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3/xps-13-%E3%83%97%E3%83%A9%E3%83%81%E3%83%8A-%E3%82%B7%E3%83%AB%E3%83%90%E3%83%BC/spd/xps-13-9305-laptop/cax6300hlits16on3ojp","</a>XPS 13</a>や<a href=","https://www.lenovo.com/jp/ja/kakaku/notebooks/ideapad/flex-5-series/IdeaPad-Flex-5-14ITL-05/p/82HS00X3JP",">IdeaPad Flex 550i</a>をおすすめします。</p>
             </details>
             <details>
             <summary>工学部</summary>
             <p>プログラミング等を行うため、他学部に比べて比較的高いスペックのPCが要求されます。<br>
-            そのため、メモリが16GB、SSD容量が512GBで、Intel Core™ i7-1165G7 を搭載した<a href=","https://www.dell.com/ja-jp/shop/cty/pdp/spd/inspiron-15-3511-laptop/cai3453hlwts16on3ojp?gacd=9643257-23732657-5785552-266271997-127759547&dgc=af&VEN1=/Vv6e0WKODg-*auGi30V6YDcCKerYpozIg&dclid=CPj1-bKCqvYCFQaNvAodUQoM7Q&nclid=DIBttjKlZEo-8QBoXQvCfzuePsqAVOgbZNvNUi6irqfJQk-ScuiSR2d27tOCOlWS#features_section","</a>Inspiron 15 3000</a>や<a href=","https://www.lenovo.com/jp/ja/kakaku/notebooks/ideapad/flex-5-series/IdeaPad-Flex-5-14ITL-05/p/82HS00X3JP",">IdeaPad Flex 550i</a>をおすすめします。</p>
+            そのため、メモリが16GB、SSD容量が512GBで、Intel Core™ i7-1165G7 を搭載した<a href=","https://www.dell.com/ja-jp/work/shop/%E3%83%87%E3%83%AB%E3%81%AE%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3/xps-13-%E3%83%97%E3%83%A9%E3%83%81%E3%83%8A-%E3%82%B7%E3%83%AB%E3%83%90%E3%83%BC/spd/xps-13-9305-laptop/cax6300hlits16on3ojp","</a>XPS 13</a>や<a href=","https://www.lenovo.com/jp/ja/kakaku/notebooks/ideapad/flex-5-series/IdeaPad-Flex-5-14ITL-05/p/82HS00X3JP",">IdeaPad Flex 550i</a>をおすすめします。</p>
             </details>
             <details>
             <summary>人文社会科学部、農学部、理学部</summary>
             <p>レポート等を書いたりするのにパソコンは必須ですがハイスペックである必要はありません<br>
             しかしながら、ある程度のスペックを確保すると快適に勉強することができるので、AMD Ryzen™ 7 5700Uを搭載した<a href=","https://deals.dell.com/ja-jp/productdetail/damd",">Inspiron 14 AMD</a>をお勧めします。</p>
+            <h2>理学部の方へ</h2>
+            <p>数式処理システムMathematicaや分子構造描画ツールChemOffice(ChemDraw)などの重たい各種アプリケーションを使うこともあるので、Corei7かRyzen7の最新世代のCPUを搭載した製品を購入すると快適に授業を受けられると思います。<br>
+            <a href=","https://www.dell.com/ja-jp/work/shop/%E3%83%87%E3%83%AB%E3%81%AE%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3/xps-13-%E3%83%97%E3%83%A9%E3%83%81%E3%83%8A-%E3%82%B7%E3%83%AB%E3%83%90%E3%83%BC/spd/xps-13-9305-laptop/cax6300hlits16on3ojp","</a>XPS 13</a>や<a href=","https://www.lenovo.com/jp/ja/kakaku/notebooks/ideapad/flex-5-series/IdeaPad-Flex-5-14ITL-05/p/82HS00X3JP",">IdeaPad Flex 550i</a>などは安定性と高性能を両立している最新世代のCorei7を搭載しているのでおすすめです。</p>
             </details>
             <details>
             <summary>教育学部、地域創造学環</summary>
@@ -188,7 +230,7 @@
             また、より作業を快適にするためにメモリ容量の多いものを購入することをおすすめします。</p>
           
             <h2>教育学部技術教育専修の人へ</h2>
-            <p>他学部に比べて高いスペックのPCが要求されます。授業を快適に行うには高いスペックのPCが要求されるため、メモリが16GB、SSD容量が512GBで、Intel Core™ i7-1165G7 を搭載した<a href=","https://www.dell.com/ja-jp/shop/cty/pdp/spd/inspiron-15-3511-laptop/cai3453hlwts16on3ojp?gacd=9643257-23732657-5785552-266271997-127759547&dgc=af&VEN1=/Vv6e0WKODg-*auGi30V6YDcCKerYpozIg&dclid=CPj1-bKCqvYCFQaNvAodUQoM7Q&nclid=DIBttjKlZEo-8QBoXQvCfzuePsqAVOgbZNvNUi6irqfJQk-ScuiSR2d27tOCOlWS#features_section","</a>Inspiron 15 3000</a>や<a href=","https://www.lenovo.com/jp/ja/kakaku/notebooks/ideapad/flex-5-series/IdeaPad-Flex-5-14ITL-05/p/82HS00X3JP",">IdeaPad Flex 550i</a>をおすすめします。<br>
+            <p>他学部に比べて高いスペックのPCが要求されます。授業を快適に行うには高いスペックのPCが要求されるため、メモリが16GB、SSD容量が512GBで、Intel Core™ i7-1165G7 を搭載した<a href=","https://www.dell.com/ja-jp/work/shop/%E3%83%87%E3%83%AB%E3%81%AE%E3%83%8E%E3%83%BC%E3%83%88%E3%83%91%E3%82%BD%E3%82%B3%E3%83%B3/xps-13-%E3%83%97%E3%83%A9%E3%83%81%E3%83%8A-%E3%82%B7%E3%83%AB%E3%83%90%E3%83%BC/spd/xps-13-9305-laptop/cax6300hlits16on3ojp","</a>XPS 13</a>や<a href=","https://www.lenovo.com/jp/ja/kakaku/notebooks/ideapad/flex-5-series/IdeaPad-Flex-5-14ITL-05/p/82HS00X3JP",">IdeaPad Flex 550i</a>をおすすめします。<br>
             </p>
             <h2>それ以外の人へ</h2>
             <p>レポート等を書いたりするのにパソコンは必須ですがハイスペックである必要はありません<br>
@@ -208,8 +250,16 @@
             <h2>坂上と坂下について</h2>
             <p>浜キャンは台地の上にあります。<br>
             その台地の上と下をつなぐ道路の坂がかなりあるため、毎日の通学を考えるとかなり大変かもしれません。<br>
-            </p>
+            下に表示されているGoogleストリートビューは浜キャンから150メートルほど離れた場所です。<br>
+            見て分かる通り、かなり急な長い坂になっています。因みに、坂の先にある建物は浜キャンの建物の一つです。</p>
             <iframe src=","https://www.google.com/maps/embed?pb=!4v1646491301177!6m8!1m7!1svODgy7NAaoLXwj9PMnF-SQ!2m2!1d34.72891481290574!2d137.7144885379222!3f153.99774243815293!4f-5.418017547228487!5f1.598266724894137"," width=","100%"," height=","450px"," style=","border:0;"," allowfullscreen=",""," loading=","lazy","></iframe>
+            <p>坂上も坂下もスーパーなどは一通り揃っているので生活の便利さに大きな差はありませんが、通学にの便利さには大きな差があると言えるでしょう。<br>
+            また、原付での通学は2Km以上でないといけないので大学近くの坂下のアパートを借りてしまうと「キツイから原付にする」という事もできません。<br>
+            <b>ただし、坂上の家賃は坂下の大体1.5倍ほどします。</b>お財布と相談しながら物件を決めてはいかがでしょうか？。<br>
+            因みに大学から浜松駅までは自転車で20分ほどです。自転車でよく駅近くに向かいたい方は浜キャンの南～東側の物件をおすすめします。<br>
+            また、下のGoogleマップの通り、スーパーなどはキャンパスの北西～北東にかけて多いです。<br>
+            ただし、キャンパスのすぐ南にあるドラッグストアは比較的食料品が豊富なのでキャンパス南に住むと買い物に苦労するという事はあまりありません。</p>
+            <iframe src=","https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d13116.714724393394!2d137.7221326507909!3d34.72589021113813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z44K544O844OR44O8!5e0!3m2!1sja!2sjp!4v1646495360235!5m2!1sja!2sjp"," width=","100%"," height=","450"," style=","border:0;"," allowfullscreen=",""," loading=","lazy","></iframe>
             
             </details>
             <details>
@@ -247,30 +297,34 @@
             <p>みんなでレクリエーションをすることによって友達作りができるかもしれません<br>
             また、先輩とも交流できるかもしれません</p>
             </details>
-            生協<br>
-            授業のガイダンス<br>
-            教科書<br>
-            プレ入学式<br>
-            
-            下宿する人向け<br>
-            静キャンに住む人<br>
-            住民票<br>
-            
-            在学生も見てほしい大学生活の情報<br>
-            サークル<br>
-            運転免許<br>
-            バイト<br>
-            静キャン前ファミマに注意<br>
-            TOEIC</p>
+           
+            <p>不安なことや慣れないことが多くて大変だと思いますが、大学生活は楽しいこともたくさんありますよ!<br>
+            なにか分からないことがあったらTwitterにいる先輩達に聞いてくださいね!<br>
+            (中の人含め）みんな喜んで答えてくれると思います！</p>
             </div>";
         };
         if("notice"==$page){
-            echo"<p>見てくれた皆さんへのお知らせ</p>";
+            echo"<div class=","notice",">
+            <h1>見てくれた皆さんへのお知らせ</h1>
+            <p>宣伝欄と更新履歴についてです。</p>
+            <h2>学情チャンプロジェクト</h2>
+            <p>学務情報システムというあまり優秀ではないことでおなじみのシステムの擬人化コンテンツの運営委員会です。<br>
+            不定期でイラストコンテストなどを開催しています。過去のコンテストについてや、公式の楽曲等について、創作ガイドライン等もあるのでぜひ<a href=","https://gakujochan.com",">学情チャンのウェブサイト</a>をチェックしてみてください。<br>
+            もしもこのプロジェクトに参加したくなった場合は<a href=","https://twitter.com/gakujochan_info",">学情チャンプロジェクトの運営Twitter</a>にDMを送ってください!<br>
+            また、ファンアートについてはいつでもお待ちしています！</p>
+            <a class=","twitter-timeline"," data-lang=","ja"," data-height=","500"," href=","https://twitter.com/Gakuzyo_chan?ref_src=twsrc%5Etfw",">Tweets by Gakuzyo_chan</a> <script async src=","https://platform.twitter.com/widgets.js"," charset=","utf-8","></script> 
+            </div>";
         };
         if("question"==$page){
-            echo"<h2>ウェブサイト運営者への質問</h2>
-            <p>質問や訂正事項,掲載してほしい情報等がありましたら、私のTwitterにダイレクトメッセージを送ってくださると幸いです。</p>
-            <a href=","https://twitter.com/messages/compose?recipient_id=&ref_src=twsrc%5Etfw"," class=","twitter-dm-button"," data-screen-name=","ghevp"," data-show-count=","false",">Message @ghevp</a><script  async  src=","https://platform.twitter.com/widgets.js","  charset=","utf-8","></script>";
+            echo"
+            <div class=","question",">
+            <h2>ウェブサイト運営者への質問</h2>
+            <p>質問や訂正事項,掲載してほしい情報等がありましたら、<a href=","https://twitter.com/ghevp",">私のTwitter</a>にダイレクトメッセージを送ってくださると幸いです。<br>
+            また、もしも何か質問があればいつでも待っているのでそちらに関してもDMへどうぞ</p>
+            <a href=","https://twitter.com/messages/compose?recipient_id=&ref_src=twsrc%5Etfw"," class=","twitter-dm-button"," data-screen-name=","ghevp"," data-show-count=","false",">Message @ghevp</a><script  async  src=","https://platform.twitter.com/widgets.js","  charset=","utf-8","></script>
+            <h2>協力者募集</h2>
+            <p>このウェブサイトでは今後も更新作業を続ける予定です。もしも更新作業に協力してくれるという方がいましたら、作業用のdiscordに招待致しますのでDMを送ってくださると幸いです。</p>
+            </div>";
         };
        
         
@@ -286,7 +340,7 @@
                     <li><a href="index.php?page=home">ホーム</a></li>
                         <li><a href="index.php?page=newstudents">新入生の皆さんへ</a></li>
                         <li><a href="index.php?page=notice">おしらせ</a></li>
-                        <li><a href="index.php?page=question">困った時</a></li><!--増やすときはボタンの使用で変更-->
+                        <li><a href="index.php?page=question">質問など</a></li><!--増やすときはボタンの使用で変更-->
                     </ul>
                 </nav>
 
